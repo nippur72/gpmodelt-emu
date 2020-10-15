a8a192b87494a06d3b68ac4d737f650981e4a8de
nino-porcino <nino.porcino@gmail.com>
2020-10-15 09:32:48 +0200

    disassemble ACI rom

--- roms/dis.bat
+++ roms/dis.bat
@@ -2,5 +2,7 @@
 
 ..\..\..\z80\yazd\yazd.exe --addr:0xe400 --xref --html --lowercase --mwr ROM_MT16_U2.BIN ROM_MT16_U2.html
 
+..\..\..\z80\yazd\yazd.exe --addr:0xec00 --xref --html --lowercase --mwr ACI_15-48_U4.BIN ACI_15-48_U4.html
+
 
 
