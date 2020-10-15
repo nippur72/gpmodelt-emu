6a3b7db30b6b1ef1843a1263caabf3fa95ce7633
nino-porcino <nino.porcino@gmail.com>
2020-10-15 10:47:51 +0200

    disassemble FDC

--- roms/dis.bat
+++ roms/dis.bat
@@ -2,6 +2,10 @@
 
 ..\..\..\z80\yazd\yazd.exe --addr:0xe400 --xref --html --lowercase --mwr ROM_MT16_U2.BIN ROM_MT16_U2.html
 
+..\..\..\z80\yazd\yazd.exe --addr:0xe800 --xref --html --lowercase --mwr FDC8-8_U3.BIN FDC8-8_U3.html
+
+..\..\..\z80\yazd\yazd.exe --addr:0xe800 --xref --html --lowercase --mwr ROM-U3.BIN ROM-U3.html
+
 ..\..\..\z80\yazd\yazd.exe --addr:0xec00 --xref --html --lowercase --mwr ACI_15-48_U4.BIN ACI_15-48_U4.html
 
 
