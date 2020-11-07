05e6cbc9cff4253fd17ef79ae64da296211dbb02
nino-porcino <nino.porcino@gmail.com>
2020-11-07 09:41:16 +0100

    Update extract_disks.js

--- tools/extract_disks.js
+++ tools/extract_disks.js
@@ -1,14 +1,48 @@
 const fs = require('fs');
-const { FDC_getpos: getpos } = require("./fdc-pos");
+const { FDC_getpos: getpos, FDC_getpos_geometry, emptydisk } = require("./fdc-pos");
 
 // read all disks
 let disk_path = "../software";
 let disk_names = fs.readdirSync(disk_path).filter(fn=>fn.match(/GP.*dsk$/g));
 
 //disk_names.forEach(dn=>extract_boot(dn));
+//disk_names.forEach(dn=>extract_ccp(dn, 0));
+//disk_names.forEach(dn=>extract_ccp(dn, 1));
 
-disk_names.forEach(dn=>extract_ccp(dn, 0));
-disk_names.forEach(dn=>extract_ccp(dn, 1));
+disk_names.forEach(dn=>extract_side_1(dn));
+
+function extract_side_1(disk_name) {
+    let geometry = {
+        TYPE8:       true,
+        TYPE525:     false,
+        NSIDES:      2,
+        NTRACKS:     77,
+        NSECTORS:    26,
+        SECTORSIZE:  128,
+        FIRSTSECTOR: 1,
+        DOUBLESIDE:  false
+    };
+
+    let source_name = `${disk_path}/${disk_name}`;
+    let dest_name = `${disk_path}/${disk_name}.side1.SS.dsk`;
+    let source_disk = fs.readFileSync(source_name);
+    let dest_disk = emptydisk(geometry);
+
+    for(let t=0;t<geometry.NTRACKS;t++) {
+        for(let s=1;s<=geometry.NSECTORS;s++) {
+            let pos_source = FDC_getpos_geometry(1,t,s,geometry);
+            let pos_dest = FDC_getpos_geometry(0,t,s,geometry);
+
+            // copy sector
+            for(i=0;i<geometry.SECTORSIZE;i++) {
+                dest_disk[pos_dest+i] = source_disk[pos_source+i];
+            }
+        }
+    }
+
+    // write
+    fs.writeFileSync(dest_name, dest_disk);
+}
 
 function extract_boot(disk_name) {
     let disk = fs.readFileSync(`${disk_path}/${disk_name}`);
@@ -41,9 +75,12 @@ function extract_ccp(disk_name, side) {
     }
 
     let ccp_name = `../docs/ccp/${disk_name}.side${side}.ccp.bin`;
-
     fs.writeFileSync(ccp_name, new Uint8Array(ccp));
-
     console.log(`extracted ${ccp_name} from ${disk_name} side ${side}`);
+
+    let cbios = ccp.slice(0x1600,0x197f);
+    let cbios_name = `../docs/ccp/${disk_name}.side${side}.cbios.bin`;
+    fs.writeFileSync(cbios_name, new Uint8Array(cbios));
+    console.log(`extracted ${cbios_name} from ${disk_name} side ${side}`);
 }
 
