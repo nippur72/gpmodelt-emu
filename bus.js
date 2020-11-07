133be6a824a0785d208a1790fd6fc54b1432b71f
nino-porcino <nino.porcino@gmail.com>
2020-11-07 09:33:06 +0100

    serial port support

--- bus.js
+++ bus.js
@@ -25,6 +25,14 @@ function io_read(ioport) {
          // VSYNC necessita bit 3 posto a 0, bit 2 posto a 1
          return 0b00000100 | (cassette_bit_in << 1);
 
+      case 0x78:
+         // serial data read
+         return serial.receive();
+
+      case 0x7a:
+         // serial status, always ready
+         return serial.receiveStatus();
+
       case 0xBC:
       case 0xBD:
       case 0xBE:
@@ -51,10 +59,30 @@ function io_write(ioport, value) {
          FDC_write_port_3f(value);
          return;
 
+      case 0x5c:
+         // parallel printer
+         printerWrite(value);
+         return;
+
+      case 0x5e:
+      case 0x5f:
+         // paralle printer config, ignored
+         return;
+
       case 0x77:
          // ACI port
          cassette_bit_out1 = (value & (1<<0)) > 0;
          cassette_bit_out2 = (value & (1<<2)) > 0;
+         return;
+
+      case 0x78:
+         // serial data
+         serial.send(value);
+         return;
+
+      case 0x7a:
+         // serial command, ignored
+         serial.readyToSend(value);
          return;
 
       case 0xBC:
@@ -70,3 +98,36 @@ function io_write(ioport, value) {
    warn(`write on unknown port ${hex(port)}h value ${hex(value)}h`);
 }
 
+class Serial
+{
+   constructor() {
+      this.output = [];
+      this.recbuf = [];
+   }
+
+   receive() {
+      if(this.recbuf.length > 0) {
+         let ch = this.recbuf[0];
+         this.recbuf = this.recbuf.slice(1);
+         return ch;
+      }
+      return 0x00; // receive buffer empty
+   }
+
+   receiveStatus() {
+      return 4+1;
+      //if(this.recbuf.length > 0) return 4+1;
+      //else return 0;
+   }
+
+   readyToSend(value) {
+
+   }
+
+   send(data) {
+      //this.output.push(data);
+      printerWrite(data);
+   }
+}
+
+serial = new Serial();
